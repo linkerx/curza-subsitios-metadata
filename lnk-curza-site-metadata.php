6321584a2d80b5c9befa080161f2216e7839d450
Diego Martinez Diaz <diego.martinezdiaz@linkerx.com.ar>
2019-06-26 10:47:44 -0300

    id_departamento en site

--- lnk-curza-site-metadata.php
+++ lnk-curza-site-metadata.php
@@ -14,10 +14,43 @@ function curza_sites_register_route(){
 
     $route = 'curza/v1';
   
-    // Endpoint: subsitio
     register_rest_route( $route, '/sites/(?P<name>[a-zA-Z0-9-]+)', array(
       'methods' => WP_REST_Server::READABLE,
       'callback' => 'curza_get_site',
     ));
   }
   add_action( 'rest_api_init', 'curza_sites_register_route');
+
+  /**
+ * Subsitio CURZA
+ *
+ * @param WP_REST_Request $request Id del sitio
+ * @return WP_REST_Response $sites Datos del sitio
+ */
+function curza_get_site(WP_REST_Request $request){
+
+    $sites_args = array(
+      'path' => '/'.$request['name'].'/' // los posts tb solo publicos?
+    );
+    $sites = get_sites($sites_args);
+    if(count($sites) != 1){
+      return new WP_REST_Response('no existe el área', 404 );
+    }
+    $site = $sites[0];
+  
+    switch_to_blog($site->blog_id);
+    $site->frontpage = get_option('page_on_front');
+    $site->tipo_pagina = get_option('curza_tipo_pagina','otro');
+    $site->id_departamento = get_option('curza_id_departamento',0);
+  
+    if($site->frontpage != 0){
+      $site->page = get_post($site->frontpage);
+    }
+  
+    $site->blog_name = get_bloginfo('name');
+    $site->blog_description = get_bloginfo('description');
+    $site->wpurl = get_bloginfo('wpurl');
+    restore_current_blog();
+  
+    return new WP_REST_Response($site, 200 );
+  }
